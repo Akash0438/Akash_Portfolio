b11debff1c1030130a1cb9191e03f08cf64fb7ca
Akash V <vuppallaakash@gmail.com>
2026-07-06 21:24:53 +0530

    github actions updates
    github actions updates

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Akash_Portfolio',
+  base: '/Akash_Portfolio/',
   plugins: [
     react(),
     tailwindcss(),
